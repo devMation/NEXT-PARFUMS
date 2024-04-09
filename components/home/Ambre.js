2217470a4b27192fd082aef2b4768fa802721a0f
amine <devprogamine@gmail.com>
2024-04-09 14:17:43 +0200

    upadate

--- components/home/Ambre.js
+++ components/home/Ambre.js
@@ -13,28 +13,14 @@ const Ambre = (props) => {
 
   console.log(Object.values(props));
 
-  const ImageHoverChange = () => {
-    const [isHovered, setIsHovered] = useState(false);
-  
-    // Fonction pour gérer le survol de l'image
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    // Fonction pour gérer la fin du survol de l'image
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    }
-  }
-
   return (
     // retourner une valeur
     // je englobe dans une div tous le processus de bouclage qui permet d'afficher mais articles
     <div>
-      <h1 className="text-center mt-4 uppercase text-2xl font-semibold tracking-normal">
+      <h1 className="text-center mt-4 uppercase text-2xl font-semibold tracking-normal  ">
         Ambre كهرمان
       </h1>
-      <div id="ambre" className="grid grid-cols-2 place-items-center">
+      <div id="ambre" className="grid grid-cols-2 place-items-center ">
         {Object.values(props).map((item, index) => (
           // Avec le .map() je vais boucler dans le tableau ambre contenu dans AmbreData.
           //item sa va chercher la valeur de mon tableau d'objet via un (.) pour aller la recuperer avec le parametre.
@@ -44,7 +30,7 @@ const Ambre = (props) => {
 
           <div
             key={index}
-            className="border-2 border-black-600 w-80 m-2 h-80  font-medium border-2   "
+            className=" my-3 brightness-100  border-2 border-black-600 w-80 m-2 h-80  font-medium border-2    shadow-lg shadow-slate-600  "
           >
             <img className="w-96 h-40 m-auto  " src={item.src} />
             <h3 className=" text-base uppercase text-center  font-normal">
@@ -58,8 +44,8 @@ const Ambre = (props) => {
             <p className=" fz-2 p-1 text-lg font-bold text-amber-300 traking-normal">
               {item.price}
             </p>
-            <div className=" w-full mt-4 text-center h-10 p-2 bg-amber-400">
-              <button  >
+            <div className=" w-full mt-2 text-center h-10 p-2 bg-amber-400 hover:text-white ">
+              <button className="hover:text-amber-600  ">
                 Ajoute au panier
               </button>
             </div>
