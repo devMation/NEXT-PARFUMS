003888a3613501cf7daffab396fdabf2ff256f45
amine <devprogamine@gmail.com>
2024-04-09 11:19:15 +0200

    update parfum

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
-
 // importer mon fichier  HomePage qui contient toute mais donnée à afficher
 
 import HomePageComponent from "../components/home/Homepage.js";
 
-// crée une fonction d'exporte 
+// crée une fonction d'exporte
 // Et déclarer mon HomePageComponent pour afficher tous mais articles
 export default function home() {
   return (
     <>
-      <h1>Salut</h1>
-
-      
       <HomePageComponent />
     </>
   );
